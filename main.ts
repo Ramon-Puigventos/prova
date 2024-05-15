e271f8024496e8316992917a36d9c7d347f68747
Ramon-Puigventos <146018688+Ramon-Puigventos@users.noreply.github.com>
2024-05-15 16:38:05 +0200

    Actualització pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-    basic.showString("Hello World!")
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # # # # #
+        . . # . .
+        . # . # .
+        `)
 })
